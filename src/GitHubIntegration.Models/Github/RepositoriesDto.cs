442152c6055a64363c8d288d5acd8d98265c7b38
lucas.franklin <lucas.franklin@riachuelo.com.br>
2024-04-26 13:33:05 -0300

    Adicionar arquivos de projeto.

---
+++ src/GitHubIntegration.Models/Github/RepositoriesDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GithubIntegration.Models.Github
+{
+    public class RepositoriesDto
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string language { get; set; }
+        public string avatar { get; set; }
+
+    }
+}
---
+++ src/GitHubIntegration/Controllers/GitHubController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using GithubIntegration.Facades.Github.Interfaces;
+
+namespace GithubIntegration.Controllers
+{
+
+    /// <summary>
+    /// Github Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GitHubController : ControllerBase
+    {
+        private readonly IGithubFacade _githubFacade;
+
+        /// <summary>
+        /// Construction class
+        /// </summary>
+        public GitHubController(IGithubFacade githubFacade)
+        {
+            _githubFacade = githubFacade;
+        }
+
+        /// <summary>
+        /// Request repository list  
+        /// </summary>
+        /// <returns>
+        /// <param name="user"></param>
+        /// <param name="quantity"></param>
+        /// <param name="sort"></param>
+        /// <param name="direction"></param>
+        /// <param name="language"></param>
+        /// </returns>
+        [HttpGet("repositories")]
+        public async Task<IActionResult> RepositoryList([Required(AllowEmptyStrings = false)] string user, [Required(AllowEmptyStrings = false)] int quantity, [Required(AllowEmptyStrings = false)] string sort, [Required(AllowEmptyStrings = false)] string direction, [Required(AllowEmptyStrings = true)] string language)
+        {
+            var value = await _githubFacade.RepositoryList(user, quantity, sort, direction, language);
+            return Ok(value);
+        }
+
+    }
+}
